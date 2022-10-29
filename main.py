e973d40fc584085579ae5720a7c5251f27471ea4
Tanyu Brandon <82066029+tanyubrandon@users.noreply.github.com>
2022-10-29 18:51:54 +0000

    print statement with concatenated strings

---
+++ main.py
@@ -0,0 +1,13 @@
+# myName = input("What's your name? ")
+# myLunch = input("What are you having for lunch? ")
+# print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+#Learning the idea behind concatenation
+
+food = input("Name a food>" )
+plant = input("Name a type of plant>")
+burnFood = input("What words describe burned food?> ")
+DIV = input(" Name a DIY item>")
+
+print("MENU")
+print("Saute ",food," with ",burnFood,plant," on a bed of ", DIV)
